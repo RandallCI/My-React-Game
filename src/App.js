0facfc2dff2ca0995b099529e0efc057d2a23da5
Randall Isaacs <sorandall777@gmail.com>
2023-05-29 05:42:24 +0200

    Thank You My GOD. I have started working in react. Thank You LORD JESUS my GOD.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import { useEffect, useState } from 'react';
+
+//Properties
+const PLAYER_SIZE = 50;
+const GAME_WIDTH = 750;
+const GAME_HEIGHT = 700;
+const GRAVITY = 9;
+const LIFT_HEIGHT = 145;
+const OBSTACLE_WIDTH = 50;
+const OBSTACLE_GAP = 150
 
 function App() {
+  //Set the player position and position the player initially.
+  const [playerPosition, setPlayerPosition] = useState(250);
+  //Set an initial start time for the game.
+  const [gameHasStarted, setGameHasStarted] = useState(false);
+  //Create the game obstacles and set their heights.
+  const [obstacleHeight, setObstacleHeight] = useState(100);
+  //Set the size and position of the left most starting point of the obstacle.
+  const [obstacleLeft, setObstacleLeft] = useState(GAME_WIDTH - OBSTACLE_WIDTH);
+  //Set the height of the obstacle at the bottom of the screen.
+  const bottomObstacleHeight = GAME_HEIGHT - obstacleHeight - OBSTACLE_GAP;
+;
+  useEffect(() => {
+    let timeId;
+    if (gameHasStarted && playerPosition < GAME_HEIGHT - PLAYER_SIZE) {
+      timeId = setInterval(() => {
+        setPlayerPosition( playerPosition => playerPosition + GRAVITY)
+      }, 24)
+    }
+    return () => {
+      clearInterval(timeId);
+    }
+  }, [playerPosition, gameHasStarted]);
+
+  useEffect(() => {
+    let obstacleID;
+    if (gameHasStarted & obstacleLeft >= -OBSTACLE_WIDTH) {
+      obstacleID = setInterval(() => {
+        setObstacleLeft((obstacleLeft) => obstacleLeft - 3.5);
+      }, 24);
+      return () => {
+        clearInterval(obstacleID);
+      };
+    } else {
+      setObstacleLeft(GAME_WIDTH - OBSTACLE_WIDTH)
+      setObstacleHeight(Math.floor(Math.random() * (GAME_HEIGHT - OBSTACLE_GAP)));
+    }
+  });
+
+  const liftPlayer = () => {
+    let newPlayerPosition = playerPosition - LIFT_HEIGHT;
+    if (!gameHasStarted) {
+      setGameHasStarted(true);
+    } else if (newPlayerPosition < 0 + PLAYER_SIZE) {
+      setPlayerPosition(0);
+    } else {
+      setPlayerPosition(newPlayerPosition);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CenteringItem className="App" onClick={liftPlayer}>
+      <GameFrame height = {GAME_HEIGHT} width = {GAME_WIDTH}>
+      {/* Set the top obstacle. */}
+      <Obstacle top = {0} height = {obstacleHeight} width = {OBSTACLE_WIDTH} left = {obstacleLeft} />
+      {/* Set the bottom obstacle. */}
+      <Obstacle top = {GAME_HEIGHT - obstacleHeight - bottomObstacleHeight} 
+      height = {bottomObstacleHeight} 
+      width = {OBSTACLE_WIDTH} 
+      left = {obstacleLeft} />
+      {/* Set th player */}
+      <Player size = {PLAYER_SIZE} top = {playerPosition}/>
+      </GameFrame>
+    </CenteringItem>
   );
+
+ 
 }
 
+
+
 export default App;
+
+
+
+//Game Objects
+
+//Create the object that navigates the scene.
+const Player = styled.div `
+position: absolute;
+background-color: red;
+height: ${(props) => props.size}px;
+width: ${(props) => props.size}px;
+top: ${(props) => props.top}px;
+border-radius: 50%;
+`;
+
+//Create the object that centre's the flying object
+const CenteringItem = styled.div `
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
+
+//Create the frame that surrounds the game scene.
+const GameFrame = styled.div `
+  height: ${(props) => props.height}px;
+  width: ${(props) => props.width}px;
+  background-color: blue;
+  overflow: hidden;
+`;
+
+//Create the obstacles which will move across the screen.
+const Obstacle = styled.div `
+  position: relative;
+  background-color: green;
+  height: ${(props) => props.height}px;
+  width: ${(props) => props.width}px;
+  top: ${(props) => props.top}px;
+  left: ${(props) => props.left}px;
+`;
